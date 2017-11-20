f26abcc3b6564fbc64cab9b8922a15c1ae3c46bc
clivemchd <clivemchd@gmail.com>
2017-11-20 14:25:29 +0530

    demoRedactor: - added plugins to accomodate different buttons.

--- public/bundle.js
+++ public/bundle.js
@@ -38994,16 +38994,22 @@ module.exports = angular;
 },{"./angular":5}],7:[function(require,module,exports){
 module.exports = function (app) {
 	app.controller('fourApp', [
-    '$scope', 
-    '$state', 
-    '$rootScope', 
-    '$cookies', 
-    '$timeout',
-    'getFactory',
-    function($scope, $state, $rootScope, $cookies, $timeout, getFactory) {		
-    	$scope.redactConfig = function () {
-				$('#redactor').redactor()
-			}
+	'$scope', 
+	'$state', 
+	'$rootScope', 
+	'$cookies', 
+	'$timeout',
+	'getFactory',
+	function($scope, $state, $rootScope, $cookies, $timeout, getFactory) {		
+		$scope.redactConfig = function () {
+			$('#redactor').redactor({		
+				plugins: ['inlinestyle', 'source', 'table', 'fullscreen', 'alignment', 'fontsize', 'imagemanager'],		
+				maxHeight: 300,
+				imageManagerJson: '/images/images.json',
+				imageUpload: '/upload.php'
+			})
+		}
+
 	}])
 }
 
--- public/components/js/four/four.js
+++ public/components/js/four/four.js
@@ -1,15 +1,21 @@
 module.exports = function (app) {
 	app.controller('fourApp', [
-    '$scope', 
-    '$state', 
-    '$rootScope', 
-    '$cookies', 
-    '$timeout',
-    'getFactory',
-    function($scope, $state, $rootScope, $cookies, $timeout, getFactory) {		
-    	$scope.redactConfig = function () {
-				$('#redactor').redactor()
-			}
+	'$scope', 
+	'$state', 
+	'$rootScope', 
+	'$cookies', 
+	'$timeout',
+	'getFactory',
+	function($scope, $state, $rootScope, $cookies, $timeout, getFactory) {		
+		$scope.redactConfig = function () {
+			$('#redactor').redactor({		
+				plugins: ['inlinestyle', 'source', 'table', 'fullscreen', 'alignment', 'fontsize', 'imagemanager'],		
+				maxHeight: 300,
+				imageManagerJson: '/images/images.json',
+				imageUpload: '/upload.php'
+			})
+		}
+
 	}])
 }
 
