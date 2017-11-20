0b5569212fcc72913c0ccdd443574d8c8d083307
clivemchd <clivemchd@gmail.com>
2017-11-20 15:54:17 +0530

    demoRedactor: - fixes correction

--- public/components/js/four/four.js
+++ public/components/js/four/four.js
@@ -9,10 +9,8 @@ module.exports = function (app) {
 	function($scope, $state, $rootScope, $cookies, $timeout, getFactory) {		
 		$scope.redactConfig = function () {
 			$('#redactor').redactor({		
-				plugins: ['inlinestyle', 'source', 'table', 'fullscreen', 'alignment', 'fontsize', 'imagemanager'],		
-				maxHeight: 300,
-				imageManagerJson: '/images/images.json',
-				imageUpload: '/upload.php'
+				plugins: ['inlinestyle', 'source', 'table', 'fullscreen', 'alignment', 'fontsize'],		
+				maxHeight: 300
 			})
 		}
 	}])
